581e0245ea5ba0d989f4544a8ec296eb6ea87ccb
wisdom oladipupo <wisbel71@gmail.com>
2024-11-14 20:17:33 +0100

    fisrt submission final  commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const burger = document.getElementById("svg")
+
+const   navlist = document.querySelector(".navList")
+
+burger.addEventListener("click", () => {
+    if(navlist.style.display === "none" || navlist.style.display === ""){
+        navlist.style.display = "block"
+    } else {
+        navlist.style.display = "none"
+    }
+})
